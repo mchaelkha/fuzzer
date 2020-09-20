014115c33c2ae2dda2acdf08d0541c87b274678a
mxk5025 <michaelkha213@gmail.com>
2020-09-20 17:37:45 -0400

    Stub fuzz.py

--- fuzzer/fuzz.py
+++ fuzzer/fuzz.py
@@ -6,7 +6,8 @@ import urllib.parse
 parser = argparse.ArgumentParser()
 
 # Browser connection to 127.0.0.1
+dvwa_addr = 'http://127.0.0.1/'
 browser = mechanicalsoup.StatefulBrowser()
-response = browser.open("127.0.0.1/")
+response = browser.open(dvwa_addr)
 print(response)
 
