d92be77181178fc5539b1c28983bf188d7caeb3e
mxk5025 <michaelkha213@gmail.com>
2020-09-20 20:36:40 -0400

    Part 0 parser

--- fuzzer/fuzz.py
+++ fuzzer/fuzz.py
@@ -1,23 +1,18 @@
-import re
 import argparse
 import mechanicalsoup
-import urllib.parse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('discover', action='store_const',
-                    help='discover a URL address argument')
-parser.add_argument('--custom-auth=dvwa', action='store_const',
-                    help='use custom authentication')
-args = parser.parse_args()
-print(args)
 
-# Browser connection to 127.0.0.1
-dvwa_addr = 'http://127.0.0.1/'
-dvwa_test = dvwa_addr + 'fuzzer-tests'
-browser = mechanicalsoup.StatefulBrowser()
-response = browser.open(dvwa_addr)
-print(response)
+def parser_init():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--custom-auth', required=False)
+    parser.add_argument('discover', type=str)
+    parser.add_argument('url', type=str)
+    return parser.parse_args()
 
-response = browser.open(dvwa_test)
-print(response)
 
+if __name__ == '__main__':
+    args = parser_init()
+
+    browser = mechanicalsoup.StatefulBrowser()
+    response = browser.open(args.url)
+    print(response)
