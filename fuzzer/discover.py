c5040e26ae928b18b32bd97794d8513157ad2caf
mxk5025 <michaelkha213@gmail.com>
2020-10-10 00:00:43 -0400

    Refactor lines into methods

--- fuzzer/discover.py
+++ fuzzer/discover.py
@@ -2,44 +2,58 @@ import mechanicalsoup
 import urllib.parse
 
 
+def dvwa_auth(url):
+    browser = mechanicalsoup.StatefulBrowser()
+    # Go to setup page and reset the database
+    browser.open(urllib.parse.urljoin(url, '/setup.php'))
+    browser.select_form('form[action="#"]')
+    browser.submit_selected()
+
+    # Go to login page and login as admin
+    browser.open(url)
+    browser.select_form('form[action="login.php"]')
+    browser['username'] = 'admin'
+    browser['password'] = 'password'
+    browser.submit_selected()
+
+    # Change security level to low
+    browser.open(urllib.parse.urljoin(url, '/security.php'))
+    browser.select_form('form[action="#"]')
+    browser['security'] = 'low'
+    browser.submit_selected()
+
+
+def read_extensions(filename, exts):
+    with open(filename) as ef:
+        for line in ef:
+            line = line.strip()
+            exts.append(line)
+
+
+def read_common_words(filename, words):
+    with open(filename) as wf:
+        for line in wf:
+            line = line.strip()
+            words.append(line)
+
+
 def discover(args):
     if args.custom_auth == "dvwa":
-        browser = mechanicalsoup.StatefulBrowser()
-        # Go to setup page and reset the database
-        browser.open(urllib.parse.urljoin(args.url, '/setup.php'))
-        browser.select_form('form[action="#"]')
-        browser.submit_selected()
-
-        # Go to login page and login as admin
-        browser.open(args.url)
-        browser.select_form('form[action="login.php"]')
-        browser['username'] = 'admin'
-        browser['password'] = 'password'
-        browser.submit_selected()
-
-        # Change security level to low
-        browser.open(urllib.parse.urljoin(args.url, '/security.php'))
-        browser.select_form('form[action="#"]')
-        browser['security'] = 'low'
-        browser.submit_selected()
+        dvwa_auth(args.url)
 
     # Populate extensions from file or with default values
     exts = []
     if args.extensions:
-        with open(args.extensions) as ef:
-            for line in ef:
-                line = line.strip()
-                exts.append(line)
+        read_extensions(args.extensions, exts)
     else:
         print("Using default file extensions...")
         exts.append('.php')
         exts.append('')
 
+    # Populate filenames from common_words file
     filenames = []
     if args.common_words:
-        with open(args.common_words) as wf:
-            for line in wf:
-                line = line.strip()
-                filenames.append(line)
+        read_common_words(args.common_words, filenames)
     else:
-        print("Missing common-words argument...")
+        print("Missing required common-words argument...")
+        exit(1)
